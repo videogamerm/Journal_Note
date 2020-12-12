be72668977c1d974ef14c12dafa5c1300f7cdcb7
videogamerm <75590266+videogamerm@users.noreply.github.com>
2020-12-12 09:02:50 -0500

    Add files via upload

--- Download.sh
+++ Download.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-git clone https://github.com/videogamerm/Journal_Note
 
+git clone https://github.com/videogamerm/Journal_Note
+sudo apt-get install python3
 
 cd Journal_Note
 rm Download.sh

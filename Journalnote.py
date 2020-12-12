3ffe5883390b1471a20f9ea7b427ebfe0ae40f28
videogamerm <75590266+videogamerm@users.noreply.github.com>
2020-12-12 08:53:46 -0500

    Add files via upload

--- Journalnote.py
+++ Journalnote.py
@@ -2,20 +2,21 @@ import datetime
 import os
 
 
+
 now = datetime.datetime.now()
-fmt = "%Y-%m-%d %H:%M:%S"
+fmt = "%Y-%m-%d-%H"
 
 
-def musicRubyrun():
-    os.system("")
+def musicrun():
+    os.system("nvlc IHVMK.mp3")
+    os.system("bg")
     
 
 
-
+os.system("touch .files")
 start = input("Type s to start a new entry, P to play music, or c to stop.")
-
 if start == "p" or "p":
-    musicRubyrun    
+    musicrun()
 if start == "s" or "S":
     cd = str(now.strftime(fmt)) + ".txt"    
     Code = open( cd, "w")

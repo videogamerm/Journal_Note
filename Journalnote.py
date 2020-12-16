7a97a325065232e2ee5c5ff4f039491d470455ad
videogamerm <75590266+videogamerm@users.noreply.github.com>
2020-12-16 18:15:06 -0500

    Add files via upload

--- Journalnote.py
+++ Journalnote.py
@@ -2,12 +2,6 @@ import datetime
 import os
 from pygame import mixer 
 
-
-
-
-
-
-
 now = datetime.datetime.now()
 fmt = "%Y-%m-%d-%H"
 
@@ -20,30 +14,46 @@ def readr():
 def readp():
     
     os.system("ls *.mp3")
-    song = input("Type song name(+Remember only mp3 so far")
+    song = input("Type song name(Remember only mp3 )")
     mixer.init() 
 
     mixer.music.load(song) 
 
     mixer.music.set_volume(0.7) 
 
-    mixer.music.play() 
+    mixer.music.play()
+def videop():
+    os.system("ls *.mp4 *.avi")
+    filemn = input("Type video name from list")
+    os.system("vlc " + filemn )
 
-start = input("Type s to start a new entry, r to read,p to play music(mp3 only), or c to stop.")
+start = input("Type s to start a new entry or edit a previosly made one, r to read,p to play music, v to play video, or c to stop. ")
 
 
 
+if start == "s":
+    s2 = input("Type a for adding text to a older file or s for new entry . ")
+    if s2 == "a":
+        os.system("ls *.txt")
+        File = input("Type your file from the list above ")
+        wdywts = input("Type addition.")
+        text = open( File, "w")
+        text.write(wdywts)
+    
+        
+    if s2 == "s":
+        cd = str(now.strftime(fmt)) + ".txt"    
+        Code = open( cd, "w")
+        Entry = input("Type  your Journal Entry. ")
+        Code.write (str(Entry))
+        Code.close()
 
-if start == "s" :
-    cd = str(now.strftime(fmt)) + ".txt"    
-    Code = open( cd, "w")
-    Entry = input("Type  your Journal Entry. ")
-    Code.write (str(Entry))
-    Code.close()
+
 if start == "r":
     readr()
 if start == "p":
     readp()
-    
+if start == "v":
+    videop()
 if start == "c":
     exit()
